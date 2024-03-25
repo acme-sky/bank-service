72f4c54476f7c1e9714410a6936e4f51950c5675
Santo Cariotti <santo@dcariotti.me>
2024-03-25 16:20:14 +0100

    Use env var

--- ui/src/stores/payment.js
+++ ui/src/stores/payment.js
@@ -4,7 +4,7 @@ export const usePaymentStore = defineStore('payment', {
   state: () => ({ data: {}, inloading: false }),
   actions: {
     async get(id) {
-      await fetch(`http://localhost:8080/payments/${id}/`).then(async (response) => {
+      await fetch(`${import.meta.env.VITE_BACKEND_URL}/payments/${id}/`).then(async (response) => {
         this.data = await response.json()
       })
       return this.data
@@ -13,7 +13,7 @@ export const usePaymentStore = defineStore('payment', {
       this.inloading = true
       let result = 400
 
-      await fetch(`http://localhost:8080/payments/${id}/pay/`, { method: 'POST' })
+      await fetch(`${import.meta.env.VITE_BACKEND_URL}/payments/${id}/pay/`, { method: 'POST' })
         .then(async (response) => {
           result = response.status
         })
